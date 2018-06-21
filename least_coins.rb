bcacb332442eea079f16a1ff0ee3a67a757ba388
ilanarubin <ilanarubin007@gmail.com>
2018-06-21 18:18:50 +0000

    Done.

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,30 @@
 #write out your code here
 
 def least_coins(cents)
+  coins= {
+      :quarters => 0,
+      :dimes => 0,
+      :nickels => 0,
+      :pennies => 0
+    }
 
-#Code your answer here!
+  while cents > 25
+    coins[:quarters] += 1
+    cents -= 25
+  end
+
+  while cents > 10
+    coins[:dimes] += 1
+    cents -= 10
+  end
+
+  while cents > 5
+    coins[:nickles] += 1
+    cents -= 5
+  end
+
+    coins[:pennies] = cents
+
+  return coins
 
 end
