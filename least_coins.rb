9e4a62f21ccc6b9e8009a89748f31404ba371023
ilanarubin <ilanarubin007@gmail.com>
2018-06-21 18:16:10 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -2,24 +2,24 @@
 
 def least_coins(cents)
   coin_types= {
-      :quarters=0
-      :dimes=0
-      :nickels=0
-      :pennies=0
+      :quarters = 0
+      :dimes = 0
+      :nickels = 0
+      :pennies = 0
   }
 
-  while cents >25
-    coins[:quarters]+=1
+  while cents > 25
+    coins[:quarters] += 1
     cents -= 25
   end
 
-  while cents >10
-    coins[:dimes]+=1
+  while cents > 10
+    coins[:dimes] += 1
     cents -= 10
   end
 
-  while cents >5
-    coins[:nickles]+=1
+  while cents > 5
+    coins[:nickles] += 1
     cents -= 5
   end
 
